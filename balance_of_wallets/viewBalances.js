69474f1dc6947f2e08e182d0577bf74a01878019
Jaime Serrano <jserrano.gonzalez.moreno@gmail.com>
2022-07-26 14:31:02 +0200

    Update viewBalances.js

--- balance_of_wallets/viewBalances.js
+++ balance_of_wallets/viewBalances.js
@@ -5,7 +5,7 @@ const matic = {
   chainId: 137,
   _defaultProvider: (providers) =>
     new providers.JsonRpcProvider(
-      "https://polygon-mainnet.g.alchemy.com/v2/bfSzgTmgSKMUvNh1nW2CiQdUKLNO9K-e"
+      "put your alchemy key here"
     ),
 };
 
